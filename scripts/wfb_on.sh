f7b3b6506b5abc585c13aa1a772b2e9b52f0bff4
amtcvx <xp31@free.fr>
2024-08-30 16:25:16 +0200

    Update wfb_on.sh

--- scripts/wfb_on.sh
+++ scripts/wfb_on.sh
@@ -7,5 +7,5 @@ PIDFILE=/tmp/wfb.pid
 sysctl -w net.ipv6.conf.all.disable_ipv6=1
 $HOME_PRJ/src/wfb > /dev/null 2>&1 &
 echo $! | tee -a $PIDFILE > /dev/null 2>&1 
-$HOME_PRJ/scripts/video.sh $PIDFILE > /dev/null 2>&1 &
-echo $! | tee -a $PIDFILE > /dev/null 2>&1 
+#$HOME_PRJ/scripts/video.sh $PIDFILE > /dev/null 2>&1 &
+#echo $! | tee -a $PIDFILE > /dev/null 2>&1 
