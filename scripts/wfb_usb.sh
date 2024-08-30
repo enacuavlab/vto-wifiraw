1d2c00551121c5d843785300e0b44395d86d4d2e
amtcvx <xp31@free.fr>
2024-08-30 16:23:34 +0200

    Add files via upload

---
+++ scripts/wfb_usb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOME_PRJ=TOBEUPDATEATINSTALLATION 
+
+USBFILE=/tmp/wfb.usb
+PIDFILE=/tmp/wfb.pid
+
+if [ -f "$USBFILE" ]; then
+  rm $USBFILE  > /dev/null 2>&1
+  if [ -f "$PIDFILE" ]; then
+    systemctl stop wfb.service
+  else
+    systemctl start wfb.service
+  fi
+else
+  echo -n >$USBFILE
+fi
